532015b93c0aba30a2a4b746b845c4cd69c21ba6
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2024-12-11 14:21:32 +0700

    Add Laravel Octane support and update paginator component; enhance .gitignore

--- resources/js/Components/Home/Paginator.jsx
+++ resources/js/Components/Home/Paginator.jsx
@@ -1,17 +1,25 @@
-import { Link } from "@inertiajs/react";
 import React from "react";
+import { Pagination } from "@mui/material";
+import { Link } from "@inertiajs/react";
 
 const Paginator = ({ meta }) => {
-    const prevPage = meta.links[0].url;
-    const nextPage = meta.links[meta.links.length - 1].url;
     const currentPage = meta.current_page;
     const lastPage = meta.last_page;
 
+    const handlePageChange = (event, page) => {
+        const link = meta.links.find((link) => link.label == page)?.url;
+        if (link) {
+            window.location.href = link; // Redirect to the selected page
+        }
+    };
+
     return (
-        <div className="join">
-            {prevPage && <Link href={prevPage} className="join-item btn">«</Link>}
-            <button className="join-item btn">{currentPage}</button>
-            {nextPage && <Link href={nextPage} className="join-item btn">»</Link>}
+        <div className="my-4 flex justify-center">
+            <Pagination
+                count={lastPage} // Total number of pages
+                page={currentPage} // Current active page
+                onChange={handlePageChange}
+            />
         </div>
     );
 };
